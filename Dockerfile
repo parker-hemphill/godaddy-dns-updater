8f8270beaa2e3e64a8c2976bc92e21e4e3570569
Parker Hemphill <parker@hemphill-server.com>
2020-05-18 16:23:01 -0400

    Fixed godaddy_dns_update.sh to use docker env variables

--- Dockerfile
+++ Dockerfile
@@ -3,26 +3,27 @@ FROM alpine:latest
 
 # Install curl
 RUN apk add --no-cache curl
+RUN apk add --no-cache bash
 
 # Set version label
-LABEL build_version="Godaddy-DNS-Updater, Version: 1.0.2, Build-date: 18-May-2020"
+LABEL build_version="Godaddy-DNS-Updater, Version: 1.0.4, Build-date: 18-May-2020"
 LABEL maintainer=parker-hemphill
 
 # Copy convert shell scripts to /opt
 RUN echo "**** copy shell scripts to /opt ****"
-COPY godaddy_dns_update.sh /
+RUN mkdir /opt
+COPY godaddy_dns_update.sh /opt/
 
 # Set scripts as executable
 RUN echo "**** set shell scripts as executable ****"
-RUN chmod +rxxx /godaddy_dns_update.sh
+RUN chmod +rxxx /opt/godaddy_dns_update.sh
 
 # Set default docker variables
 RUN echo "**** setup default variables****"
+ENV DOMAIN=${DOMAIN:-NULL}
+ENV SUB_DOMAIN=${SUB_DOMAIN:-@}
+ENV API_KEY=${API_KEY:-NULL}
+ENV DNS_CHECK=${DNS_CHECK:-900}
 ENV TZ=${TZ:-America/New_York}
-ENV DOMAIN=${DOMAIN:-@}
-ENV HOSTNAME=${HOSTNAME:-NULL}
-ENV API=${API:-NULL}
-ENV CHECK=${CHECK:-900}
 
-
-ENTRYPOINT ["/godaddy_dns_update.sh"]
+ENTRYPOINT ["/usr/bin/bash /opt/godaddy_dns_update.sh ${DOMAIN} ${SUB_DOMAIN} ${API_KEY} ${DNS_CHECK} ${TZ}"]
--- godaddy_dns_update.sh
+++ godaddy_dns_update.sh
@@ -1,36 +1,42 @@
 #!/usr/bin/bash
 
-mydomain=${DOMAIN}
-myhostname=${HOSTNAME}
-gdapikey=${API}
-logdest="/tmp/godaddy_update.log"
+domain="$1"
+subdomain="$2"
+apikey="$3"
+dns_check="${4}"
+logdest="/dev/shm/godaddy_update.log"
 
-ENV CHECK=${CHECK:-900}
+# Set timezone for container
+export TZ="${5}"
 
 # Check if variables are set and exit if not
-if [[ ${DOMAIN} == 'NULL' ]]
+if [[ ${1} == 'NULL' ]]
 then
   echo "DOMAIN not set, exiting container"
+  echo "DOMAIN not set, exiting container" >> ${logdest}
   exit 1
 fi
-if [[ ${API} == 'NULL' ]]
+if [[ ${3} == 'NULL' ]]
 then
-  echo "API not set, exiting container"
+  echo "API_KEY not set, exiting container"
+  echo "API_KEY not set, exiting container" >> ${logdest}
   exit 1
 fi
 
-# Loop to check DNS record for update, based on ${CHECK} variable (in seconds)
-while :; do
-sleep ${CHECK}
-  # Random sleep so multiple site updates doesn't cause API to timeout
+echo "Monitoring records for ${subdomain}.${domain}, checks every ${dns_check} seconds"
+echo "Monitoring records for ${subdomain}.${domain}, checks every ${dns_check} seconds" >> ${logdest}
+
+while :;
+do
+  sleep ${dns_check}
+  #Random sleep so multiple site updates doesn't cause API to timeout
   sleep $((RANDOM % 15 + 1))
-  # DNS checks
   myip=`curl -s "https://api.ipify.org"`
-  dnsdata=`curl -s -X GET -H "Authorization: sso-key ${gdapikey}" "https://api.godaddy.com/v1/domains/${mydomain}/records/A/${myhostname}"`
+  dnsdata=`curl -s -X GET -H "Authorization: sso-key ${apikey}" "https://api.godaddy.com/v1/domains/${domain}/records/A/${subdomain}"`
   gdip=`echo $dnsdata | cut -d ',' -f 1 | tr -d '"' | cut -d ":" -f 2`
   if [ "$gdip" != "$myip" -a "$myip" != "" ]; then
-    curl -s -X PUT "https://api.godaddy.com/v1/domains/${mydomain}/records/A/${myhostname}" -H "Authorization: sso-key ${gdapikey}" -H "Content-Type: application/json" -d "[{\"data\": \"${myip}\"}]"
-    echo "Changed IP on ${hostname}.${mydomain} from ${gdip} to ${myip}" >> $logdest
-    echo "Changed IP on ${hostname}.${mydomain} from ${gdip} to ${myip}"
+    curl -s -X PUT "https://api.godaddy.com/v1/domains/${domain}/records/A/${subdomain}" -H "Authorization: sso-key ${apikey}" -H "Content-Type: application/json" -d "[{\"data\": \"${myip}\"}]"
+    echo "Changed IP on ${subdomain}.${domain} from ${gdip} to ${myip}"
+    echo "Changed IP on ${subdomain}.${domain} from ${gdip} to ${myip}" >> ${logdest}
   fi
 done
